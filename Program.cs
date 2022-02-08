6c948965372e25aae76d37cc0b548063e6c9034a
PedroXaviiier <phxavier.phx@gmail.com>
2022-02-08 14:03:39 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um
+algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno.
+*/
+
+
+
+using System;
+
+namespace Dimenção_do_terreno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o comprimento do terreno: ");
+
+            double Comprimento = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a largura do terreno: ");
+
+            double Largura = Convert.ToDouble(Console.ReadLine());
+
+            if (Comprimento == Largura)
+            {
+                Console.WriteLine("Dimensão do terreno só pode ser retangular");
+            }
+            else {
+                double Area = Comprimento * Largura;
+                Console.WriteLine(Area);
+            }
+            
+        }
+    }
+}
